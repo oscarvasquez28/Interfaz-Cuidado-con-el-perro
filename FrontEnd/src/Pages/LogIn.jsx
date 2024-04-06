9ca244a798f8728cea53c425eaf64267c3869477
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2024-04-06 17:24:01 -0600

    Arerglos esteticos de log in

--- FrontEnd/src/Pages/LogIn.jsx
+++ FrontEnd/src/Pages/LogIn.jsx
@@ -3,8 +3,6 @@ import './SignIn.css';
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 
-// import bfooter from '../Images/Footer/background-footer.jpg'
-
 function SignIn(){
   const [showPass, setShowPass] = useState(false)
   const [email, setEmail] = useState('');
@@ -17,72 +15,61 @@ function SignIn(){
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
-  
     return (
       <div>
+        <div className='LogIn-container'>
+          <header className='header'>
+            <Header/>
+          </header>
+          <div className='both-sides'> 
 
-        <header className='header'>
-          <Header/>
-        </header>
-        
-        <div className='both-sides'> 
+            <div className='left-side'>
+              <div className='general'>
 
-          <div className='left-side'>
-            <div className='general'>
-
-              <div className='general-title'>
-                <h2 className='right-side-subtitle'>
-                  ENTRA A TU CUENTA
-                </h2>            
-              </div>
-
-              <form action=""> {/* aqui empieza el forms-----------*/}
-
-                <div className=''>
-                  <input type="text" className='email-subscribe-si' placeholder='Email' value={email} onChange={handleEmailChange}/>
+                <div className='general-title'>
+                  <h2 className='right-side-subtitle'>
+                    ENTRA A TU CUENTA
+                  </h2>            
                 </div>
 
-                <div className=''>        
-                  <div className='pass-input'>
-                    <input type={ showPass ? "text" : "password"}       className='email-subscribe-si' placeholder='Contraseña' value={password} 
-                    onChange={handlePasswordChange}/>
+                <form action=""> {/* aqui empieza el forms-----------*/}
 
-                    <div className='pass-icon' onClick={() => setShowPass (!showPass)}>
-                    {showPass ? <i class="bi bi-eye"></i> : <i class="bi bi-eye-slash"></i>}
-                    </div>
+                  <div className=''>
+                    <input type="text" className='email-subscribe-si' placeholder='Email' value={email} onChange={handleEmailChange}/>
+                  </div>
 
-                  </div>   
-                </div>  
+                  <div className=''>        
+                    <div className='pass-input'>
+                      <input type={ showPass ? "text" : "password"}       className='email-subscribe-si' placeholder='Contraseña' value={password} 
+                      onChange={handlePasswordChange}/>
 
+                      <div className='pass-icon' onClick={() => setShowPass (!showPass)}>
+                      {showPass ? <i class="bi bi-eye"></i> : <i class="bi bi-eye-slash"></i>}
+                      </div>
+                    </div>   
+                  </div>  
+                  <div>
+                    <button className={ (password !== '' && email !== '') ? "btn-create-si" : "btn-unirse-si"} type='submit'>INICIAR SESIÓN</button>
+                  </div> 
+                </form>                
                 <div>
-                  <button className={ (password !== '' && email !== '') ? "btn-create-si" : "btn-unirse-si"} type='submit'>INICIAR SESIÓN</button>
-                </div> 
-              </form>                
-
-              <div>
-                <p className='or-reset-pass'>¿OLVISASTE TU CONTRASEÑA?</p>
-              </div>
-            
+                  <p className='or-reset-pass'>¿OLVISASTE TU CONTRASEÑA?</p>
+                </div>         
+              </div>        
             </div>
-          
-          </div>
-
-          <div className='right-side'>
-            <h3 className='right-side-subtitle'>
-              ¿NO TIENES CUENTA?<br />
-              REGÍSTRATE Y HAZ TUS COMPRAS MÁS RÁPIDO.
-            </h3>
-
-            <div>
-              <button className='btn-create-si'>CREAR CUENTA</button>
+            <div className='right-side'>
+              <h3 className='right-side-subtitle'>
+                ¿NO TIENES CUENTA?<br />
+                REGÍSTRATE Y HAZ TUS COMPRAS MÁS RÁPIDO.
+              </h3>
+              <div>
+                <button className='btn-create-si'>CREAR CUENTA</button>
+              </div>
             </div>
           </div>
         </div>
-
         <Footer/>
-
       </div>
-
     );
 }
 
