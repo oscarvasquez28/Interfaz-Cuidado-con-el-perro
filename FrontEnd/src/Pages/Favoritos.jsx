9e50d6b38a4cd54725c4e82db0ddfff2406be030
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2024-05-10 14:48:43 -0600

    Diseño basico pagina favoritos

--- FrontEnd/src/Pages/Favoritos.jsx
+++ FrontEnd/src/Pages/Favoritos.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Link} from 'react-router-dom';
 import Header from '../Components/Header'
+import UserContext from '../Context/UserContext'; // Importa el contexto
 
 function Favoritos() {
+  const { userData } = useContext(UserContext); // Cambia username a userData
+  const userId = userData.id; // Obtén el ID del contexto de usuario
+  const username = userData.username;
   return (
-    <div>
+    <div className='all-container'>
         <Header/>
+        <div className='up'>
+        <div>
+        <p className='title'> FAVORITOS (0) </p>
+        <Link to ={`/leerCarrito/${userId}`}><p className='title-muted'> CARRITO</p></Link>
+        </div>
+        </div>
     </div>
   )
 }
